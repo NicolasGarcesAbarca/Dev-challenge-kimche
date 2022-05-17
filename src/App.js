1b404d851364bde62bb5b1359a9ce5e075169a34
nico <nickogarces@gmail.com>
2022-05-17 11:21:47 -0400

    fixgit add eset of previous template and update of project to CRA react-scripts 5.0.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
-import "./App.css";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
+import logo from './logo.svg';
+import './App.css';
 
-const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
-});
-
-const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>
-        My first Apollo app{" "}
-        <span role="img" aria-label="Rocket">
-          🚀
-        </span>
-      </h2>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </ApolloProvider>
-);
+  );
+}
+
 export default App;
