877811f90adbf2e38a66b0139e088888f3c1f92e
nico <nickogarces@gmail.com>
2022-05-19 18:39:48 -0400

    style: improve style of ResultPanel and GroupedCard

--- src/components/GroupedCards/GroupedCards.js
+++ src/components/GroupedCards/GroupedCards.js
@@ -4,20 +4,29 @@ import { Card } from './Card';
 
 const CardContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 300px));
+  grid-template-columns: repeat(auto-fit, minmax(150px, 300px));
   gap:1rem;
 `;
 
+const GroupedCardsContainer = styled.section`
+  padding: 0 4rem;
+  @media (min-width: 1000px) {
+    padding: 0 3rem;
+  }
+  & > h2 {
+    margin: 4rem 0 1rem 0;
+  }
+`
 function GroupedCards({ name, data }) {
   return (
-    <>
+    <GroupedCardsContainer>
       <h2>{name}</h2>
       <CardContainer>
         {data.length && data.map((country) => (
           <Card key={country.name} countryData={country} />
         ))}
       </CardContainer>
-    </>
+    </GroupedCardsContainer>
   );
 }
 
--- src/components/ResultPanel/ResultPanel.js
+++ src/components/ResultPanel/ResultPanel.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import { GroupedCards } from '../GroupedCards';
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;  
+`
 
 function ResultPanel({ data }) {
   return (
-    <div>
-      <h2>ResultPanel</h2>
+    <Container>
       {Object.keys(data).map((key) => <GroupedCards key={key} name={key} data={data[key]} />)}
-    </div>
+    </Container>
   );
 }
 
